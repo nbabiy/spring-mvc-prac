7345850e8cfd2bf116d563a183d142e543b2d540
Nazarii Babii <n.babiy69@gmail.com>
2021-07-22 11:59:27 +0300

    Hello end-point accepts two params: name and surname

--- src/main/java/com/nbabiy/controllers/FirstController.java
+++ src/main/java/com/nbabiy/controllers/FirstController.java
@@ -3,6 +3,9 @@ package com.nbabiy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author nazariibabii
@@ -12,7 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+//        @RequestParam throws 400 BadRequest if one or more params are not exits
+//        but you can use required param of that annotation to make params not required
+
+//        You can use HttpServletRequest as argument and spring autowire it
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname );
+
         return "first/hello";
     }
 
