ae6f570e0ca5d8598ac8890ba3bd67a275f93623
Nazarii Babii <n.babiy69@gmail.com>
2021-07-23 09:44:22 +0300

    Add page for creating new Person

--- src/main/java/com/nbabiy/controllers/PeopleController.java
+++ src/main/java/com/nbabiy/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.nbabiy.controllers;
 
 import com.nbabiy.dao.PersonDAO;
+import com.nbabiy.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author nazariibabii
@@ -35,6 +34,17 @@ public class PeopleController {
         // receives a person from DB and passes it to model for viewing
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 
 }
